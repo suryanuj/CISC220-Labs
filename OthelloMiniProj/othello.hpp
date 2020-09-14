c88b79e22b81019a4c963ec090a5d55a8cba2ece
suryanuj <suryanuj@udel.edu>
2020-09-13 22:36:11 -0400

    updated

--- OthelloMiniProj/othello.hpp
+++ OthelloMiniProj/othello.hpp
@@ -25,5 +25,26 @@ class Othello {
 
 };
 
+char makemat()
+{
+	char matrix[8][8];
+
+	for(int i=0;i<8;i++){
+	      for(int j=0;j<8;j++){
+	        matrix[i][j]='_';
+	      }
+	matrix[3][4]='O';
+    matrix[3][3]='B';
+    matrix[4][4]='B';
+    matrix[4][3]='O';
+
+    //cout << matrix <<endl;
+    return matrix;
+};
+
+void printmat()
+{
+	cout << matrix <<endl;
+}
 
 #endif /* OTHELLO_HPP_ */
