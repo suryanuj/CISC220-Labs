dd81c49366dc18f3202db98213c08f3170257252
suryanuj <suryanuj@udel.edu>
2020-09-15 17:15:36 -0400

    updated

--- OthelloMiniProj/othello.hpp
+++ OthelloMiniProj/othello.hpp
@@ -13,61 +13,65 @@
 #include "player.cpp"
 using namespace std;
 
+static int boardSize = 8;
+
 class Othello {
-	char board[][];
-	int boardSize = 8;
-	Player player1;
-	Player player2;
-	int numPlayers;
+
+		char matrix[9][9];
+		Player player1;
+		Player player2;
+		int numPlayers;
 
 	public:
 		Othello();
 		Othello(string st1, char c);
 		Othello(string st1, char c, string st2, char c2);
 
+
+
+	void makemat(){
+		//char matrix[9][9];
+
+		for(int i=1;i<9;i++){
+			  for(int j=1;j<9;j++){
+				matrix[i][j]='_';
+			  }
+		}
+
+		matrix[4][5]='O';
+		matrix[4][4]='B';
+		matrix[5][5]='B';
+		matrix[5][4]='O';
+
+		//cout << matrix <<endl;
+		//return matrix;
+
+	}
+
+	void printmat(){
+
+		string charString="-01234567";
+		for(int i=0; i<9; i++){
+			matrix[0][i]=charString[i];
+		}
+
+		for(int i=1; i<9; i++){
+			matrix[i][0]=charString[i];
+		}
+
+		int counter=0;
+		for(int i=0; i<8; i++) {
+			for(int j=0; j<8; j++) {
+				cout << matrix[i][j]<<"\t";
+				counter++;
+
+				if(counter%9==0){
+					cout << endl;
+				}
+			}
+		}
+	}
+
 };
-
-void makemat()
-{
-	char matrix[9][9];
-
-	for(int i=1;i<9;i++){
-	      for(int j=1;j<9;j++){
-	        matrix[i][j]='_';
-	      }
-	}
-
-	matrix[4][5]='O';
-    matrix[4][4]='B';
-    matrix[5][5]='B';
-    matrix[5][4]='O';
-
-    //cout << matrix <<endl;
-    //return matrix;
-
-}
-
-void printmat()
-{
-	string charString="-01234567";
-	for(int i=0; i<9; i++){
-		matrix[0][i]=charString[i];
-	}
-
-	for(int i=1; i<9; i++){
-		matrix[i][0]=charString[i];
-	}
-
-	int counter=0;
-	for(int i=0; i<8; i++) {
-		for(int j=0; j<8; j++) {
-			cout << matrix[i][j]<<"\t";
-	    		counter++;
-	      	if(counter%8==0){
-	          cout << endl;
-	 		}
-	  }
-	}
-}
 
 #endif /* OTHELLO_HPP_ */
