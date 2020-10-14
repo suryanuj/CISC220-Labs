ad76478b2a9a8d8731fb6613e41b91311245ee30
nickperugini <71109517+nickperugini@users.noreply.github.com>
2020-10-13 23:52:56 -0400

    Update DLL.cpp

--- Project2/DLL.cpp
+++ Project2/DLL.cpp
@@ -46,6 +46,7 @@ using namespace std;
 
 		while (tmp != NULL) {
 				tmp->song->printSong();
+				tmp=tmp->next;
 
 		}
 	}
@@ -104,26 +105,105 @@ using namespace std;
 		for (tmp = first;  tmp->song->title != t; tmp = tmp->next){
 			if(tmp->prev==NULL;){
 				DNode *ogfirst=first;
-				remove(first->song->title);
+
+				remove(first->song->title);		//removes original first
+
+				first=ogfirst->next;	//Sets new first
+				first->prev=NULL;		//Sets new first prev to null
+
 				push(ogfirst->song->title,ogfirst->song->artist,ogfirst->song->min,ogfirst->song->sec);
 				delete ogfirst;
 				}
+
 			tmp=tmp->next;
 		}
 		swapWith=tmp->prev;
-		if(swapWith->prev!=NULL){
-			swapWith->prev->next=tmp;
+		if(swapWith->prev!=NULL){			//Condition if node and swap node are in between two others
+			/*swapWith->prev->next=tmp;
 			tmp->next->prev=swapWith;
 			tmp->prev=swapWith->prev;
 			swapWith->prev=tmp;
 			swapWith->next=tmp->next;
 			tmp->next=swapWith;
+			*/
+			DNode *a=tmp->prev->prev;
+			DNode *b=tmp->prev;
+			DNode *c=tmp;
+			DNode *d=tmp->next;
+
+			a->next=c;
+			c->prev=a;
+			c->next=b;
+			b->prev=c;
+			b->next=d;
+			d->prev=b;
 		}
-		else{
-			tmp->next->prev=swapWith;
+		else{								//Condition if node and swap node only have one node next to them
+			/*tmp->next->prev=swapWith;
 			tmp->prev=swapWith->prev;
 			swapWith->prev=tmp;
 			swapWith->next=tmp->next;
 			tmp->next=swapWith;
+			*/
+
+			DNode *a=tmp->prev;
+			DNode *b=tmp;
+			DNode *c=tmp->next;
+
+			a->next=c;
+			a->prev=b;
+			c->prev=a;
+			b->prev=NULL;
+			b->next=a;
 		}
 	}
+	void DLL::moveDown(string t){
+			DNode *tmp;
+			DNode *swapWith;
+			int counter=0;
+			for (tmp = first;  tmp->song->title != t; tmp = tmp->next){
+				if(tmp->next==NULL;){
+					DNode *oglast=last;
+					DNode *firstHolder=first;
+
+					remove(last->song->title);		//Removes last Node
+
+					last=oglast->prev;				//Sets last Node to node to previous to last node
+					last->next=NULL;
+
+					oglast->next=first->next;		//Sets last Node in list to first Node in list
+					first=oglast;
+					first->prev=NULL;
+
+					delete oglast;
+					delete firstHolder;
+					}
+				tmp=tmp->next;
+			}
+			swapWith=tmp->next;
+			if(swapWith->next!=NULL){			//Condition if node and swap node are in between two others
+				DNode *a=tmp->next->next;
+				DNode *b=tmp->next;
+				DNode *c=tmp;
+				DNode *d=tmp->prev;
+
+				a->prev=c;
+				c->next=a;
+				c->prev=b;
+				b->next=c;
+				b->prev=d;
+				d->next=b;
+
+			}
+			else{							//Condition if node and swap node only have one node next to them
+				DNode *a=tmp->next;
+				DNode *b=tmp;
+				DNode *c=tmp->prev;
+
+				a->prev=c;
+				c->next=a;
+				b->next=NULL;
+				b->prev=a;
+				a->next=b;
+			}
+		}
